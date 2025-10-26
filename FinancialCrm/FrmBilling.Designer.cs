03626ae69dad3ae1cc1f24e92d0063ee1654d305
LUTFU <dolaylutfu@gmail.com>
2025-10-26 18:02:06 +0300

    update project

---
+++ FinancialCrm/FrmBilling.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace FinancialCrm
+{
+    partial class FrmBilling
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBillId = new System.Windows.Forms.TextBox();
+            this.txtBillTiltle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtBillAmount = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtBillPeriod = new System.Windows.Forms.Label();
+            this.btnBillList = new System.Windows.Forms.Button();
+            this.btnCreateBill = new System.Windows.Forms.Button();
+            this.btnRemoveBill = new System.Windows.Forms.Button();
+            this.btnUpdateBill = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 529);
+            this.panel1.TabIndex = 1;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(43, 378);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(134, 55);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Ayarlar";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(43, 439);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(134, 55);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Çıkış Yap";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(43, 256);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(134, 55);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Banka Hareketleri";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(43, 317);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 55);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Dashboard";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(43, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 55);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Bankalar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(43, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Faturalar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Giderler";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kategoriler";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.btnUpdateBill);
+            this.panel2.Controls.Add(this.btnRemoveBill);
+            this.panel2.Controls.Add(this.btnCreateBill);
+            this.panel2.Controls.Add(this.btnBillList);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.txtBillPeriod);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.txtBillAmount);
+            this.panel2.Controls.Add(this.txtBillTiltle);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtBillId);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(235, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1006, 269);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Location = new System.Drawing.Point(235, 300);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1006, 217);
+            this.panel3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ödeme Id :";
+            // 
+            // txtBillId
+            // 
+            this.txtBillId.Location = new System.Drawing.Point(89, 14);
+            this.txtBillId.Name = "txtBillId";
+            this.txtBillId.Size = new System.Drawing.Size(229, 20);
+            this.txtBillId.TabIndex = 1;
+            // 
+            // txtBillTiltle
+            // 
+            this.txtBillTiltle.Location = new System.Drawing.Point(89, 40);
+            this.txtBillTiltle.Name = "txtBillTiltle";
+            this.txtBillTiltle.Size = new System.Drawing.Size(229, 20);
+            this.txtBillTiltle.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Başlık :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(229, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // txtBillAmount
+            // 
+            this.txtBillAmount.AutoSize = true;
+            this.txtBillAmount.Location = new System.Drawing.Point(24, 76);
+            this.txtBillAmount.Name = "txtBillAmount";
+            this.txtBillAmount.Size = new System.Drawing.Size(42, 13);
+            this.txtBillAmount.TabIndex = 4;
+            this.txtBillAmount.Text = "Miktar :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(224, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // txtBillPeriod
+            // 
+            this.txtBillPeriod.AutoSize = true;
+            this.txtBillPeriod.Location = new System.Drawing.Point(24, 112);
+            this.txtBillPeriod.Name = "txtBillPeriod";
+            this.txtBillPeriod.Size = new System.Drawing.Size(45, 13);
+            this.txtBillPeriod.TabIndex = 6;
+            this.txtBillPeriod.Text = "Periyot :";
+            // 
+            // btnBillList
+            // 
+            this.btnBillList.Location = new System.Drawing.Point(52, 155);
+            this.btnBillList.Name = "btnBillList";
+            this.btnBillList.Size = new System.Drawing.Size(134, 55);
+            this.btnBillList.TabIndex = 8;
+            this.btnBillList.Text = "Ödeme Listesi";
+            this.btnBillList.UseVisualStyleBackColor = true;
+            this.btnBillList.Click += new System.EventHandler(this.btnBillList_Click);
+            // 
+            // btnCreateBill
+            // 
+            this.btnCreateBill.Location = new System.Drawing.Point(210, 155);
+            this.btnCreateBill.Name = "btnCreateBill";
+            this.btnCreateBill.Size = new System.Drawing.Size(134, 55);
+            this.btnCreateBill.TabIndex = 9;
+            this.btnCreateBill.Text = "Yeni Ödeme";
+            this.btnCreateBill.UseVisualStyleBackColor = true;
+            this.btnCreateBill.Click += new System.EventHandler(this.btnCreateBill_Click);
+            // 
+            // btnRemoveBill
+            // 
+            this.btnRemoveBill.Location = new System.Drawing.Point(375, 155);
+            this.btnRemoveBill.Name = "btnRemoveBill";
+            this.btnRemoveBill.Size = new System.Drawing.Size(134, 55);
+            this.btnRemoveBill.TabIndex = 10;
+            this.btnRemoveBill.Text = "Ödeme Sil";
+            this.btnRemoveBill.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateBill
+            // 
+            this.btnUpdateBill.Location = new System.Drawing.Point(530, 155);
+            this.btnUpdateBill.Name = "btnUpdateBill";
+            this.btnUpdateBill.Size = new System.Drawing.Size(134, 55);
+            this.btnUpdateBill.TabIndex = 11;
+            this.btnUpdateBill.Text = "Ödeme Güncelle";
+            this.btnUpdateBill.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1006, 214);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // FrmBilling
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 554);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmBilling";
+            this.Text = "FrmBilling";
+            this.Load += new System.EventHandler(this.FrmBilling_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label txtBillPeriod;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label txtBillAmount;
+        private System.Windows.Forms.TextBox txtBillTiltle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBillId;
+        private System.Windows.Forms.Button btnBillList;
+        private System.Windows.Forms.Button btnUpdateBill;
+        private System.Windows.Forms.Button btnRemoveBill;
+        private System.Windows.Forms.Button btnCreateBill;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ FinancialCrm/FrmBilling.cs
@@ -0,0 +1,32 @@
+﻿using FinancialCrm.Models;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FinancialCrm
+{
+    public partial class FrmBilling : Form
+    {
+        public FrmBilling()
+        {
+            InitializeComponent();
+        }
+        FinancialCrmDBEntities db = new FinancialCrmDBEntities();
+        private void FrmBilling_Load(object sender, EventArgs e)
+        {
+            var values =db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnBillList_Click(object sender, EventArgs e)
+        {
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnCreateBill_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
