171b2a4ca6dec3c3f5d5913decadfe5aec591ee7
LUTFU <dolaylutfu@gmail.com>
2025-10-29 15:09:46 +0300

    update project

--- FinancialCrm/FrmDashboard.Designer.cs
+++ FinancialCrm/FrmDashboard.Designer.cs
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea13 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend13 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea14 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend14 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.lblIsBankBalance = new System.Windows.Forms.Label();
+            this.lblLastBankProcessAmount = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.lblVakıfBankBalance = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblBillAmount = new System.Windows.Forms.Label();
+            this.lblBillTitle = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblTotalBalance = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -48,70 +55,82 @@
             this.button3 = new System.Windows.Forms.Button();
             this.btnBillForm = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel5.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.panel5.Controls.Add(this.lblIsBankBalance);
+            this.panel5.Controls.Add(this.lblLastBankProcessAmount);
             this.panel5.Controls.Add(this.label7);
             this.panel5.Location = new System.Drawing.Point(948, 75);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(304, 144);
             this.panel5.TabIndex = 8;
             // 
-            // lblIsBankBalance
+            // lblLastBankProcessAmount
             // 
-            this.lblIsBankBalance.AutoSize = true;
-            this.lblIsBankBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblIsBankBalance.Location = new System.Drawing.Point(15, 91);
-            this.lblIsBankBalance.Name = "lblIsBankBalance";
-            this.lblIsBankBalance.Size = new System.Drawing.Size(92, 25);
-            this.lblIsBankBalance.TabIndex = 1;
-            this.lblIsBankBalance.Text = "0,00 TL";
+            this.lblLastBankProcessAmount.AutoSize = true;
+            this.lblLastBankProcessAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLastBankProcessAmount.Location = new System.Drawing.Point(15, 91);
+            this.lblLastBankProcessAmount.Name = "lblLastBankProcessAmount";
+            this.lblLastBankProcessAmount.Size = new System.Drawing.Size(92, 25);
+            this.lblLastBankProcessAmount.TabIndex = 1;
+            this.lblLastBankProcessAmount.Text = "0,00 TL";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(28, 13);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.Size = new System.Drawing.Size(94, 13);
             this.label7.TabIndex = 0;
-            this.label7.Text = "İş Bankası";
+            this.label7.Text = "Son Gelen Havale";
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.SandyBrown;
-            this.panel4.Controls.Add(this.lblVakıfBankBalance);
-            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.lblBillAmount);
+            this.panel4.Controls.Add(this.lblBillTitle);
             this.panel4.Location = new System.Drawing.Point(614, 75);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(328, 144);
             this.panel4.TabIndex = 9;
             // 
-            // lblVakıfBankBalance
+            // lblBillAmount
             // 
-            this.lblVakıfBankBalance.AutoSize = true;
-            this.lblVakıfBankBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblVakıfBankBalance.Location = new System.Drawing.Point(15, 91);
-            this.lblVakıfBankBalance.Name = "lblVakıfBankBalance";
-            this.lblVakıfBankBalance.Size = new System.Drawing.Size(92, 25);
-            this.lblVakıfBankBalance.TabIndex = 1;
-            this.lblVakıfBankBalance.Text = "0,00 TL";
+            this.lblBillAmount.AutoSize = true;
+            this.lblBillAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBillAmount.Location = new System.Drawing.Point(15, 91);
+            this.lblBillAmount.Name = "lblBillAmount";
+            this.lblBillAmount.Size = new System.Drawing.Size(92, 25);
+            this.lblBillAmount.TabIndex = 1;
+            this.lblBillAmount.Text = "0,00 TL";
             // 
-            // label4
+            // lblBillTitle
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 13);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Vakıfbank";
+            this.lblBillTitle.AutoSize = true;
+            this.lblBillTitle.Location = new System.Drawing.Point(28, 13);
+            this.lblBillTitle.Name = "lblBillTitle";
+            this.lblBillTitle.Size = new System.Drawing.Size(70, 13);
+            this.lblBillTitle.TabIndex = 0;
+            this.lblBillTitle.Text = "Fatura Başlığı";
             // 
             // panel2
             // 
@@ -250,11 +269,95 @@
             this.button1.Text = "Kategoriler";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea13.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea13);
+            legend13.Name = "Legend1";
+            this.chart1.Legends.Add(legend13);
+            this.chart1.Location = new System.Drawing.Point(284, 292);
+            this.chart1.Name = "chart1";
+            series13.ChartArea = "ChartArea1";
+            series13.Legend = "Legend1";
+            series13.Name = "Series1";
+            this.chart1.Series.Add(series13);
+            this.chart1.Size = new System.Drawing.Size(376, 300);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea14.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea14);
+            legend14.Name = "Legend1";
+            this.chart2.Legends.Add(legend14);
+            this.chart2.Location = new System.Drawing.Point(752, 292);
+            this.chart2.Name = "chart2";
+            series14.ChartArea = "ChartArea1";
+            series14.Legend = "Legend1";
+            series14.Name = "Series1";
+            this.chart2.Series.Add(series14);
+            this.chart2.Size = new System.Drawing.Size(376, 300);
+            this.chart2.TabIndex = 11;
+            this.chart2.Text = "chart2";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Location = new System.Drawing.Point(284, 225);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(376, 61);
+            this.panel6.TabIndex = 12;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel7.Controls.Add(this.label4);
+            this.panel7.Location = new System.Drawing.Point(752, 225);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(376, 61);
+            this.panel7.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(17, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(319, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bankalar ve Banka Hesaplarındaki para miktarlarına\r\n aşağıdaki grafikten ulaşabil" +
+    "irsiniz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(29, 14);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(319, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bankalar ve Banka Hesaplarındaki para miktarlarına\r\n aşağıdaki grafikten ulaşabil" +
+    "irsiniz";
+            // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 603);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
@@ -272,17 +375,23 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label lblIsBankBalance;
+        private System.Windows.Forms.Label lblLastBankProcessAmount;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Label lblVakıfBankBalance;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBillAmount;
+        private System.Windows.Forms.Label lblBillTitle;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label lblTotalBalance;
         private System.Windows.Forms.Label label2;
@@ -297,6 +406,14 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button btnBillForm;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- FinancialCrm/FrmDashboard.cs
+++ FinancialCrm/FrmDashboard.cs
@@ -18,10 +18,37 @@ namespace FinancialCrm
             InitializeComponent();
         }
         FinancialCrmDBEntities db = new FinancialCrmDBEntities();
+        int count = 0;
         private void FrmDashboard_Load(object sender, EventArgs e)
         {
             var totalBalance = db.Banks.Sum(a => a.BankBalance);
             lblTotalBalance.Text = totalBalance.ToString();
+
+            var lastBankProcessAmount = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Select(y => y.Amount).FirstOrDefault();
+            lblLastBankProcessAmount.Text = lastBankProcessAmount.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (count %4== 1)
+            {
+                var suFaturasi = db.Bills.Where(x => x.BillTitle == "Su Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "Su Faturası";
+                lblBillAmount.Text = suFaturasi.ToString();
+            }
+            if (count % 4 == 2)
+            {
+                var doğalgazFaturasi = db.Bills.Where(x => x.BillTitle == "Doğalgaz Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "Doğalgaz Faturası";
+                lblBillAmount.Text = doğalgazFaturasi.ToString();
+            }
+            if (count % 4 == 3)
+            {
+                var internetFaturasi = db.Bills.Where(x => x.BillTitle == "İnternet Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "İnternet Faturası";
+                lblBillAmount.Text = internetFaturasi.ToString();
+            }
         }
     }
 }
